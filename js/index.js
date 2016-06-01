bff8f75648aa87f5ce99153fb5f8fad88a96de8f
meredal <mldalenburg@gmail.com>
2016-06-01 08:39:13 +0700

    Install and use "ScrollTo" for each down arrow on the page to go to the next section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
   $(".down-arrow").on("click", function(){
-    location.href = $(this).("attr.next");
+      // <a href="#gravida" class="down-arrow" next="gravida">
+      //    <img src="/images/content-arrow.png" alt="down" class="arrow-image">
+      // </a>
+      // <section id="gravida"></section>
+
+    var nextSectionId = $(this).attr("next");
+    var nextSection = $('#' + nextSectionId);
+      // next section is now returned with "#" in front of it to signal an id selector
+
+    $(window).scrollTo(nextSection, {duration:800});
+      // scrollTo the next section on the page, duration of 800ms
+
+    return false;
   });
 });
