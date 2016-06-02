d6067fedc8d05ed8eafefa2db6187d92ed8d8515
meredal <mldalenburg@gmail.com>
2016-06-02 11:45:37 +0700

    Add waypoints for each section of page content. When you're on that section, it is highlighted on the sidebar.

--- js/index.js
+++ js/index.js
@@ -14,22 +14,63 @@ $( document ).ready(function() {
   });
 
 // Add a waypoint to the page-content section and make it so the sidebar sticks to the side of the page when you scroll past the "Waypoint". Add the class to the sidebar to give it a fixed position.
-  var torsoWaypoint = new Waypoint({
+  var stickySidebar = new Waypoint({
     element: $('#torso')[0],
     handler: function() {
-      console.log('torso waypoint triggered');
+      // console.log('torso waypoint triggered');
       $('#sidebar').addClass('menu-fixed');
     }
   })
 
 // Add a waypoint to the header section and make it so the sidebar no longer sticks to the side of the page when you scroll past the "Waypoint". Remove the class that makes it have a fixed position.
-  var headerWaypoint = new Waypoint({
+  var unStickySidebar = new Waypoint({
     element: $('#lorem-dolor')[0],
     handler: function() {
-      console.log('header waypoint triggered');
+      // console.log('header waypoint triggered');
       $('#sidebar').removeClass('menu-fixed');
     },
     offset: 'bottom-in-view'
+  })
+
+// Use waypoints to detect when you're in a new section. As you enter a new section in page content, it should be highlighted on the sidebar.
+  var loremDolor = new Waypoint({
+    element: $('#lorem-dolor')[0],
+    handler: function() {
+      $('#sidebar .active').removeClass('active');
+      $('#sidebar .lorem-dolor').addClass('active');
+    }
+  })
+
+  var gravida = new Waypoint({
+    element: $('#gravida')[0],
+    handler: function() {
+      $('#sidebar .active').removeClass('active');
+      $('#sidebar .gravida').addClass('active');
+    }
+  })
+
+  var utSagittis = new Waypoint({
+    element: $('#ut-sagittis')[0],
+    handler: function() {
+      $('#sidebar .active').removeClass('active');
+      $('#sidebar .ut-sagittis').addClass('active');
+    }
+  })
+
+  var signUp = new Waypoint({
+    element: $('#sign-up')[0],
+    handler: function() {
+      $('#sidebar .active').removeClass('active');
+      $('#sidebar .sign-up').addClass('active');
+    }
+  })
+
+  var velBibendum = new Waypoint({
+    element: $('#vel-bibendum')[0],
+    handler: function() {
+      $('#sidebar .active').removeClass('active');
+      $('#sidebar .vel-bibendum').addClass('active');
+    }
   })
 
 });
